28cc1afbc1b473ff4d76a28ff349fed0bf33eb0f
Victor <freelance2@eviciti.com.mx>
2024-11-20 05:01:00 -0600

    Agregar archivos de proyecto.

---
+++ Service/NotificationService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.JSInterop;
+
+public class NotificationService
+{
+    private readonly IJSRuntime _jsRuntime;
+
+    public NotificationService(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+    public async Task ShowNotificationAsync(string title, string message)
+    {
+        var options = new
+        {
+            body = message,
+            //icon = "/icon.png"
+        };
+
+        await _jsRuntime.InvokeVoidAsync("showNotification", title, options);
+    }
+}
---
+++ wwwroot/site.js
@@ -0,0 +1,9 @@
+﻿function showNotification(title, options) {
+    if (Notification.permission === 'granted') {
+        navigator.serviceWorker.getRegistration().then(registration => {
+            registration.showNotification(title, options);
+        });
+    } else {
+        console.warn('Notificaciones no habilitadas');
+    }
+}
